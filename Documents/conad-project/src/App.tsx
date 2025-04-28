c26b4f92bb4514a13adf24c659a6529b1c82c3a3
Jessica Mura <jessicamura93@gmail.com>
2025-04-29 01:18:05 +0200

    edits

--- Documents/conad-project/src/App.tsx
+++ Documents/conad-project/src/App.tsx
@@ -1,30 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-<form>
-  <label htmlFor="name"></label>
-  <input type="text" id="name" name="name" value="Nome"/>
-  <label htmlFor="name"></label>
-  <input type="text" id="cognome" name="cognome" value="Cognome"/>
-  <br/>
-  <label htmlFor="email"></label>
-  <input type="email" id="email"name="email" value="Indirizzo Email"/>
-  <br/>
-  <label htmlFor="email"></label>
-  <input type="email" id="conferma indirizzo email" value="Conferma indirizzo email"/>
-  <br/>
-  <label htmlFor="password"></label>
-  <input type="password"id="password"name="password" value="Password"/>
-  <br/>
-  <label htmlFor="password"></label>
-  <input type="password"id="conferma password"name="conferma password" value="Conferma password"/>
-  <input type="submit"value="submit"/> 
-</form> 
+    <div className="container">
+      <form>
+        <div className="row">
+          <label htmlFor="name"></label>
+          <input type="text" id="name" name="name" placeholder="Nome" />
+          <label htmlFor="surname"></label>
+          <input
+            type="text"
+            id="cognome"
+            name="cognome"
+            placeholder="Cognome"
+          />
+        </div>
+        <form>
+          <div className="bottom-text">Campo obbligatorio</div>
+          <br />
+          <div className="rows">
+            <label htmlFor="email"></label>
+            <div className="indirizzo-email">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Indirizzo Email"
+              />
+            </div>
+            <br />
+            <div className="conferma-indirizzo-email">
+              <label htmlFor="email"></label>
+              <input
+                type="email"
+                id="conferma indirizzo email"
+                placeholder="Conferma indirizzo email"
+              />
+            </div>
+          </div>
+          <br />
+          <br />
+          <div className="password">
+            <label htmlFor="password"></label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <br />
+          <div className="conferma-password">
+            <label htmlFor="password"></label>
+            <input
+              type="password"
+              id="conferma password"
+              name="conferma password"
+              placeholder="Conferma password"
+            />
+          </div>
+          <input type="submit" value="continue" />
+        </form>
+      </form>
+    </div>
   );
-  };
-
+}
 
 export default App;
--- Documents/conad-project/src/component/ConadNavbar.tsx
+++ Documents/conad-project/src/component/ConadNavbar.tsx
@@ -2,22 +2,31 @@ import React from "react";
 
 const ConadNavbar = () => {
   return (
-     <div className="App">
-  <div className="Navbar">
-    <div className="title-logo">
-    <img className="rt022-picture rt022-picture--init rt022-picture--cover rt051-header__logoStandard "
-     style={{objectPosition: "center center"}} 
-     src="/assets/icons/conad-logo.svg?_u=e442f961aafe40e5fe7c95186a2850aa925c8284" 
-     data-component="" alt="Conad - persone oltre le cose" id="rt022-picture-0"/>
-  </div>
-<div className="first-row">
-  <img src="https://cdn-icons-png.flaticon.com/128/271/271220.png" loading="lazy"
-    alt="Left arrow " title="Left arrow " />
-    
-    <div className="first-text">I tuoi dati</div>
-</div>
-<div className="second-row">Tutti i campi sono obbligatori</div>
-</div>
-</div>
+    <div className="App">
+      <div className="Navbar">
+        <div className="title-logo">
+          <img
+            className="rt022-picture rt022-picture--init rt022-picture--cover rt051-header__logoStandard "
+            style={{ objectPosition: "center center" }}
+            src="/assets/icons/conad-logo.svg?_u=e442f961aafe40e5fe7c95186a2850aa925c8284"
+            data-component=""
+            alt="Conad - persone oltre le cose"
+            id="rt022-picture-0"
+          />
+        </div>
+        <div className="first-row">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/271/271220.png"
+            loading="lazy"
+            alt="Left arrow "
+            title="Left arrow "
+          />
+
+          <div className="first-text">I tuoi dati</div>
+        </div>
+        <div className="second-row">Tutti i campi sono obbligatori</div>
+      </div>
+    </div>
   );
 };
+export default ConadNavbar;
